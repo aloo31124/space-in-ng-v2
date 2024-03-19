279ff9b116ecd137666b715a70b4a933470600b4
loulou <aloo31124@gmail.com>
2024-03-19 14:23:50 +0800

    feat [空間總覽] [剩餘空間] 抽出service, 定義假資料格式

---
+++ src/app/review-room/models/rate.model.ts
@@ -0,0 +1,4 @@
+export class RateModel {
+    date = "";
+    rate = 100;
+}
--- src/app/review-room/services/review-room.service.ts
+++ src/app/review-room/services/review-room.service.ts
@@ -12,8 +12,32 @@ export class ReviewRoomService {
     private cloudFunService: CloudFunService,
   ) { }
 
+  /*
+   * 取得 booking 變化趨勢 
+   */
   getChartByTimeType(timeType:string): Observable<ChartTimeModel> {
     return this.cloudFunService.getBookingTrend(timeType);
   }
- 
+  
+  /*
+   * 取得 booking 每日剩餘空間 
+   * 資料格式:
+    [
+      {date:"2024-3-15", rate: 10},
+      {date:"2024-3-3", rate: 20},
+    ];
+   */
+  currentMonthRoomRemainingRoomRate =
+  [
+    {date:"2024-3-15", rate: 10},
+    {date:"2024-3-3", rate: 20},
+    {date:"2024-3-17", rate: 40},
+    {date:"2024-3-7", rate: 90},
+    {date:"2024-2-1", rate: 42},
+    {date:"2024-2-21", rate: 11},
+  ];
+  getBookingRate() {
+    return this.currentMonthRoomRemainingRoomRate;
+  }
+
 }
