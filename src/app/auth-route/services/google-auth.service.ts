92c5b0d0c47c4b6129fa360a67b8d79899806ecc
loulou <aloo31124@gmail.com>
2024-04-08 13:25:01 +0800

    refactor [登入] [google auth] 首次登入存入 firestore user 表

--- src/app/auth-route/services/google-auth.service.ts
+++ src/app/auth-route/services/google-auth.service.ts
@@ -3,7 +3,6 @@ import { Router } from '@angular/router';
 import { GoogleAuth } from '@codetrix-studio/capacitor-google-auth';
 import { GoogleAuthUser } from '../models/google-auth-user.model';
 import { UserService } from 'src/app/common/user/services/user-service.service';
-import { User } from 'src/app/common/user/models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -35,6 +34,15 @@ export class GoogleAuthService {
   }
 
   /*
+   * 登出 
+   */
+  logout() {
+    GoogleAuth.signOut();
+    this.currentUser = new GoogleAuthUser({});
+    this.isLoginIn = false;
+  }
+
+  /*
    * 登入 
    */
   async login() {
@@ -44,20 +52,12 @@ export class GoogleAuthService {
       this.userService
         .getUserIdByMail(user["email"])
         .then(doc => {
-
-          const newUser = {
-            userFirestoreId: doc.docs[0]._key.path.segments[6],
-            ...user,
-          }
-          this.currentUser = new GoogleAuthUser(newUser);
-          console.log(this.currentUser);
-          if (user) {
-            alert(user.email + " 成功登入!");
-            this.isLoginIn = true;
-            this.router.navigate(["home"]);
-          }
-          else {
-            alert("無法取得使用者資訊");
+          console.log(doc.docs[0]);
+          if(!doc.docs[0]) {
+            this.registerUserForFirstLogin(doc, user);
+          } else {
+            this.setCurrentUser(doc, user);
+            this.checkLogin(user);
           }
         });
     }
@@ -71,14 +71,46 @@ export class GoogleAuthService {
   }
 
   /*
-   * 登出 
+   * google auth 成功,
+   * 且首次登入, 註冊進 firestore user 表 
+   * 依照 email 新增一筆
    */
-  logout() {
-    GoogleAuth.signOut();
-    this.currentUser = new GoogleAuthUser({});
-    this.isLoginIn = false;
+  registerUserForFirstLogin(doc: any, user: any) {
+    this.userService
+      .addUser(user)
+      .then(() => {
+        alert("首次 google 授權, 註冊成功, 請再次登入");
+        //this.setCurrentUser(doc, user);
+        this.router.navigate(["/"]);
+      });
   }
 
+  /*
+   * 取得 userFirestoreId 並設定 current user
+   */
+  setCurrentUser(doc:any, user:any) {
+    const userFirestoreId = doc.docs[0]._key.path.segments[6];
+    const newUser = {
+      userFirestoreId: userFirestoreId,
+      ...user,
+    }
+    this.currentUser = new GoogleAuthUser(newUser);
+  }
+  
+  /*
+   * 檢查是否成功登入 
+   */
+  checkLogin(user: any) {
+    console.log(this.currentUser);
+    if (this.currentUser) {
+      alert(user.email + " 成功登入!");
+      this.isLoginIn = true;
+      this.router.navigate(["home"]);
+    }
+    else {
+      alert("無法取得使用者資訊");
+    }
+  }
 
   /*
    * 取得測試登入帳號 
